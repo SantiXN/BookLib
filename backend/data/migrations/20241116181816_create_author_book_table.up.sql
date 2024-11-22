dc09998962b768fb165c146e6703d66616a70771
SantiXN <santixm123@gmail.com>
2024-11-22 12:23:20 +0300

    main Поправил ошибку в форматировании

--- backend/data/migrations/20241116181816_create_author_book_table.up.sql
+++ backend/data/migrations/20241116181816_create_author_book_table.up.sql
@@ -4,7 +4,7 @@ CREATE TABLE `author_book`
     `book_id`   INT NOT NULL,
     PRIMARY KEY (`author_id`, `book_id`),
     CONSTRAINT `author_book_author_id_author_fk` FOREIGN KEY (`author_id`) REFERENCES `author` (`id`) ON DELETE CASCADE,
-    CONSTRAINT `author_book_book_id_book_fk` FOREIGN KEY (book_id) REFERENCES `book` (`id`) ON DELETE CASCADE
+    CONSTRAINT `author_book_book_id_book_fk` FOREIGN KEY (`book_id`) REFERENCES `book` (`id`) ON DELETE CASCADE
 ) ENGINE = InnoDB
   CHARACTER SET = utf8mb4
   COLLATE utf8mb4_unicode_ci

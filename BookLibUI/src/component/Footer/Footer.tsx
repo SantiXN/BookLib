0083959236510ee9130979823bfa761ea2258906
impronez <orzaev7312@gmail.com>
2024-11-29 12:20:14 +0300

    Добавил footer

---
+++ BookLibUI/src/component/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import logo from "../../images/svg/logotype.svg"
+import s from "./Footer.module.css"
+
+const Footer = () => {
+    return (
+        <footer className={s.footer}>
+            <a href="/" className={s.logoLink}>
+                <p className={s.logoText}>© BookLib 2024</p>
+                <img src={logo} className={s.logoImage}></img>
+            </a>
+        </footer>
+    )
+}
+
+export default Footer;

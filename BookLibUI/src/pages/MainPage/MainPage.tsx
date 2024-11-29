b1859ffd146f875d2805cdb7b33e41b883988047
impronez <orzaev7312@gmail.com>
2024-11-29 12:56:43 +0300

    Добавил основной блок с книгами

--- BookLibUI/src/pages/MainPage/MainPage.tsx
+++ BookLibUI/src/pages/MainPage/MainPage.tsx
@@ -1,16 +1,28 @@
 import s from './MainPage.module.css'
 import BookCard from '../../component/common/BookCard/BookCard'
 
+const books = Array(12).fill({
+    title: "Луна в ореховой скорлупе Луна в ореховой скорлупе",
+    author: "Анастасия Пикина Анастасия Пикина",
+    coverImage: "book.jpg",
+    rating: 4.8,
+    toDirect: '/',
+});
+
 const MainPage = () => {
     return (
         <div className={s.mainPageContainer}>
-            <BookCard 
-                title={"Луна в ореховой скорлупе Луна в ореховой скорлупе"} 
-                author={"Анастасия Пикина Анастасия Пикина"} 
-                coverImage={"book.jpg"} 
-                rating={4.8} 
-                toDirect={'/'}
-            />
+            {books.map((book, index) => (
+                <BookCard 
+                    key={index}
+                    title={book.title} 
+                    author={book.author} 
+                    coverImage={book.coverImage} 
+                    rating={book.rating} 
+                    toDirect={book.toDirect}
+                    classname={s.mainPageItem}
+                />
+            ))}
         </div>
     )
 }

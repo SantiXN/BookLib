2d6c87530bf175b62bd2000f42c9a45d29e950be
SantiXN <santixm123@gmail.com>
2024-11-29 13:59:57 +0300

    main Добавил доменный сервис для book

--- backend/pkg/domain/service/book.go
+++ backend/pkg/domain/service/book.go
@@ -1,14 +1,52 @@
 package service
 
+import "booklib/pkg/domain/model"
+
 type BookData struct {
 	Title       string
 	Description *string
 	CoverPath   *string
+	AuthorIDs   []int
+	CategoryIDs []int
 }
-
-//TODO: подумать какие действия будут совершаться?
 
 type BookService interface {
 	CreateBook(bookData BookData) (int, error)
 	RemoveBook(id int) error
+}
+
+func NewBookService(
+	bookRepository model.BookRepository,
+) BookService {
+	return &service{
+		repo: bookRepository,
+	}
+}
+
+type service struct {
+	repo model.BookRepository
+}
+
+func (s *service) CreateBook(bookData BookData) (int, error) {
+	id, err := s.repo.NextID()
+	if err != nil {
+		return 0, err
+	}
+	book := model.NewBook(
+		id,
+		bookData.Title,
+		bookData.Description,
+		bookData.CoverPath,
+		bookData.AuthorIDs,
+		bookData.CategoryIDs,
+	)
+	if err = s.repo.Store(book); err != nil {
+		return 0, err
+	}
+
+	return id, nil
+}
+
+func (s *service) RemoveBook(id int) error {
+	return s.repo.Remove(id)
 }

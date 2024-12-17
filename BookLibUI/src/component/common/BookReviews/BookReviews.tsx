384c4b926ccae5d2c23fdceb7945c5b369fe6105
impronez <orzaev7312@gmail.com>
2024-12-17 16:52:00 +0300

    Добавил открытие формы отзыва при нажатии на кнопку

--- BookLibUI/src/component/common/BookReviews/BookReviews.tsx
+++ BookLibUI/src/component/common/BookReviews/BookReviews.tsx
@@ -1,16 +1,23 @@
+import { useState } from 'react';
 import s from './BookReviews.module.css'
+import ReviewForm from './ReviewForm';
 
 interface BookReviewsProps {
     id: string
 }
 
 const BookReviews: React.FC<BookReviewsProps> = ({ id }) => {
+    const [isReviewFormOpen, setIsReviewFormOpen] = useState(false);
+    
+    const openReviewForm = () => setIsReviewFormOpen(true);
+    const closeReviewForm = () => setIsReviewFormOpen(false);
     //TODO: подгружать отзывы по ID книги
     return (
         <div className={s.bookReviewsContainer}>
             <div className={s.bookReviewsHeader}>
                 <p className={s.bookReviewsHeaderTitle}>Отзывы</p>
-                <button className={s.bookReviewsAddReviewButton}>Оставить отзыв</button>
+                <button className={s.bookReviewsAddReviewButton} onClick={openReviewForm}>Оставить отзыв</button>
+                <ReviewForm isOpen={isReviewFormOpen} onClose={closeReviewForm} />
             </div>
             <div className={s.reviews}>
                 <div className={s.review}>

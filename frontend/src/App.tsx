0bce457c580e668ebcd0e18e9e860d3b3f11f307
Kaldyn2004 <kirillkroptov2016@gmail.com>
2025-01-10 18:22:36 +0300

    Поправил получение страниц по роутам

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,42 +4,12 @@ import s from './app.module.css';
 import Footer from './component/Footer/Footer.tsx';
 import Header from './component/Header/Header.tsx';
 import AuthorPage from './pages/AuthorPage/AuthorPage.tsx';
+import AdminMenu from './pages/AdminPages/AdminMenu/AdminMenu.tsx';
+import EditorMenu from './pages/AdminPages/EditorMenu/EditorMenu.tsx';
+import ArticlePage from './pages/ArticlePage/ArticlePage.tsx';
+import BookCardPagePage from './pages/BookPage/BookCardPage.module.css';
+import MainPage from './pages/MainPage/MainPage.tsx';
 
-const PersonalAccount = () => {
-    return (
-        <div>
-            <h1>Личный кабинет</h1>
-            {/* Контент личного кабинета */}
-        </div>
-    );
-};
-
-const Book = () => {
-    return (
-        <div>
-            <h1>Книга ID: </h1>
-            {/* Дополнительная информация о книге */}
-        </div>
-    );
-};
-
-const Info = () => {
-    return (
-        <div>
-            <h1>Информация</h1>
-            {/* Информация о приложении или другие детали */}
-        </div>
-    );
-};
-
-const AdminEditor = () => {
-    return (
-        <div>
-            <h1>Административная панель</h1>
-            {/* Функции для администрирования */}
-        </div>
-    );
-};
 
 const App = () => {
   return (
@@ -47,13 +17,13 @@ const App = () => {
         <Header />
           <div className={s.container}>
               <Routes>
-                  <Route path="/account" element={<PersonalAccount />} />
-                  <Route path="/book/:id" element={<Book />} />
-                  <Route path="/info" element={<Info />} />
-                  <Route path="/admin" element={<AdminEditor />} />
+                  <Route path="/" element={<MainPage />} />
+                  <Route path="/book" element={<BookCardPagePage />} />
+                  <Route path="/admin" element={<AdminMenu />} />
+                  <Route path="/editor" element={<EditorMenu />} />
+                  <Route path="/author" element={<AuthorPage />} />
+                  <Route path="/article" element={<ArticlePage />} />
               </Routes>
-                
-            <AuthorPage />
           </div>
         <Footer />
       </Router>

07bbcf24e65f260049f647222fc6762c80fab703
impronez <orzaev7312@gmail.com>
2025-01-10 16:13:49 +0300

    Сгенерированная апи

---
+++ frontend/api/models/index.ts
@@ -0,0 +1,22 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AddBookRequest';
+export * from './AddBookResponseData';
+export * from './AuthorInfo';
+export * from './BadRequestResponseData';
+export * from './BookData';
+export * from './BookInfo';
+export * from './CategoryInfo';
+export * from './ChangeReadingStatusRequest';
+export * from './FeedbackInfo';
+export * from './GetBookInfoResponseData';
+export * from './ListBookFeedbackResponseData';
+export * from './ListBooksResponseData';
+export * from './NotFoundResponseData';
+export * from './PermissionDeniedResponseData';
+export * from './SaveBookFeedbackRequest';
+export * from './SaveBookFeedbackResponseData';
+export * from './SearchBooksByOptionResponseData';
+export * from './SearchBooksRequest';
+export * from './SearchBooksResponseData';
+export * from './UnauthorizedResponseData';

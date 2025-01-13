6859b89bead615457abb5bed173d3b0816ea024d
impronez <orzaev7312@gmail.com>
2025-01-13 15:55:30 +0300

    Добавил параметр feedbackInfo и получение информации о пользователе с сервера

--- frontend/src/component/common/BookReviews/BookReviews.tsx
+++ frontend/src/component/common/BookReviews/BookReviews.tsx
@@ -1,42 +1,77 @@
-import { useState } from 'react';
-import s from './BookReviews.module.css'
+import { useState, useEffect } from 'react';
+import s from './BookReviews.module.css';
 import ReviewForm from './ReviewForm';
+import { ParsedFeedbackInfo } from '../../../types/FeedbackTypes';
+import { UserApiClient } from '../../../../api/ApiClient';
+import { GetUserDataRequest } from '../../../../api';
 
 interface BookReviewsProps {
-    id: string
+    feedbackInfo: ParsedFeedbackInfo[];
 }
 
-const BookReviews: React.FC<BookReviewsProps> = ({ id }) => {
+const BookReviews: React.FC<BookReviewsProps> = ({ feedbackInfo }) => {
     const [isReviewFormOpen, setIsReviewFormOpen] = useState(false);
-    
+    const [userNames, setUserNames] = useState<Record<number, string>>({});
 
+    useEffect(() => {
+        const fetchUserNames = async () => {
+            const names: Record<number, string> = {};
+
+            // Выполняем запросы параллельно
+            const userRequests = feedbackInfo.map((review) => {
+                const request: GetUserDataRequest = { userID: review.userID };
+                return UserApiClient.getUserData(request)
+                    .then((response) => {
+                        names[review.userID] = `${response.data?.firstName} ${response.data?.lastName}`;
+                    })
+                    .catch(() => {
+                        names[review.userID] = "Unknown User";
+                    });
+            });
+
+            await Promise.all(userRequests); 
+            setUserNames(names); 
+        };
+
+        fetchUserNames();
+    }, [feedbackInfo]); 
 
     const openReviewForm = () => setIsReviewFormOpen(true);
     const closeReviewForm = () => setIsReviewFormOpen(false);
-    //TODO: подгружать отзывы по ID книги
+
     return (
         <div className={s.bookReviewsContainer}>
             <div className={s.bookReviewsHeader}>
                 <p className={s.bookReviewsHeaderTitle}>Отзывы</p>
-                <button className={s.bookReviewsAddReviewButton} onClick={openReviewForm}>Оставить отзыв</button>
+                <button className={s.bookReviewsAddReviewButton} onClick={openReviewForm}>
+                    Оставить отзыв
+                </button>
                 <ReviewForm isOpen={isReviewFormOpen} onClose={closeReviewForm} />
             </div>
             <div className={s.reviews}>
-                <div className={s.review} id={id}>
-                    <div className={s.reviewHeader}>
-                        <img className={s.reviewAvatar} src='/src/images/avatar/image.png'/>
-                        <p className={s.reviewAuthor}>Василий Пупкин</p>
-                        <span className={s.reviewDate}>22 ноября 2024</span>
-                        <div className={s.reviewRatingContainer}>
-                            <div className={s.reviewRatingLogo} />
-                            <span className={s.reviewRating}>5</span>
+                {feedbackInfo.length > 0 ? (
+                    feedbackInfo.map((review) => (
+                        <div className={s.review} key={review.id}>
+                            <div className={s.reviewHeader}>
+                                <img className={s.reviewAvatar} src='/src/images/avatar/image.png' alt="Avatar" />
+                                <p className={s.reviewAuthor}>
+                                    {userNames[review.userID] || "Loading..."}
+                                </p>
+                                <span className={s.reviewDate}>{review.postedAt}</span>
+                                <div className={s.reviewRatingContainer}>
+                                    <div className={s.reviewRatingLogo} />
+                                    <span className={s.reviewRating}>{review.starCount}</span>
+                                </div>
+                            </div>
+                            <p className={s.reviewBody}>{review.comment}</p>
                         </div>
-                    </div>
-                    <p className={s.reviewBody}>Книга понравилась. Несколько сказок в одной книге. Красивые иллюстрации. При прочтении у ребенка лишь положительные эмоции.</p>
-                </div>
+                    ))
+                ) : (
+                    <div>Отзывов нет</div>
+                )}
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default BookReviews;

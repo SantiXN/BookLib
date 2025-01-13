be059cbf37684bdc01d0f3aaa63324f676567f51
impronez <orzaev7312@gmail.com>
2025-01-13 15:57:21 +0300

    Разделил тип ParsedBook на ParsedBookCard и ParsedBookInfo

--- frontend/src/types/BookTypes.ts
+++ frontend/src/types/BookTypes.ts
@@ -1,7 +1,14 @@
-export type ParsedBook = {
+export type ParsedBookCard = {
     title: string;
     author: string;
     coverImage: string;
     rating: number;
     toDirect: string;
+};
+
+export type ParsedBookInfo = {
+    title: string;
+    description: string;
+    coverImage: string;
+    rating: number;
 };

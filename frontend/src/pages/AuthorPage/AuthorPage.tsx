2a78bbd0243645b4a2470bff314650871353873b
impronez <orzaev7312@gmail.com>
2025-01-13 13:17:27 +0300

    Сделал бету-версию получения данных для странички автора

--- frontend/src/pages/AuthorPage/AuthorPage.tsx
+++ frontend/src/pages/AuthorPage/AuthorPage.tsx
@@ -1,43 +1,79 @@
 import BookCard from '../../component/common/BookCard/BookCard';
 import s from './AuthorPage.module.css'
-
-const books = Array(12).fill({
-    title: "Луна в ореховой скорлупе Луна в ореховой скорлупе",
-    author: "Анастасия Пикина Анастасия Пикина",
-    coverImage: "book.jpg",
-    rating: 4.8,
-    toDirect: '/',
-});
+import { ParsedBook } from '../../types/BookTypes';
+import { parseBooksResponse } from '../../utils/BookUtils';
+import { useEffect, useState } from 'react';
+import {BookApiClient, AuthorApiClient } from '../../../api/ApiClient';
+import { ListAuthorBooksRequest, GetAuthorInfoRequest } from '../../../api';
+import { useParams } from 'react-router-dom';
+import { parseAuthorResponse } from '../../utils/AuthorUtils';
+import { ParsedAuthor } from '../../types/AuthorTypes';
 
 const AuthorPage = () => {
+    const [authorBooks, setAuthorBooks] = useState<ParsedBook[]>([]);
+    const [authorInfo, setAuthorInfo] = useState<ParsedAuthor>();
+
+    const { id } = useParams<{ id: string }>();
+    const curAuthorID = id ? Number(id) : 0;
+
+    const getAuthorInfoRequest: GetAuthorInfoRequest = { authorID: curAuthorID};
+    const getAuthorBooksRequest: ListAuthorBooksRequest = { authorID: curAuthorID };
+
+    useEffect(() => {
+        AuthorApiClient.getAuthorInfo(getAuthorInfoRequest)
+            .then((response) => {
+                console.log('id: ', id)
+                const curAuthorInfo = parseAuthorResponse(response);
+                if (!authorInfo) {
+                    console.error(`Failed to fetch author info ${curAuthorID}:`);
+                }
+                else {
+                    setAuthorInfo(curAuthorInfo!);
+                }
+            }) 
+
+        BookApiClient.listAuthorBooks(getAuthorBooksRequest)
+            .then((response) => {
+                if (response && response.books) {
+                    setAuthorBooks(parseBooksResponse(response));
+                    console.log('length ', authorBooks.length)
+                }
+            }) 
+    }, [curAuthorID]);
+
     return (
         <div className={s.authorContainer}>
             <div className={s.authorBody}>
-                <img className={s.authorImage} src='src/images/avatar/image.png'/>
+                <img className={s.authorImage} src='src/images/avatar/image.png' alt=''/>
                 <div className={s.authorInfo}>
-                    <p className={s.authorName}>Вин дизель</p>
+                    <p className={s.authorName}>{`${authorInfo?.firstName} ${authorInfo?.lastName}`}</p>
                     <p className={s.authorDescription}>
-                        Пишет про машины.Пишет про машины.Пишет про машины.Пишет про машины.Пишет про машины.Пишет про машины.Пишет про машины.Пишет про машины.Пишет про машины.Пишет про машины.Пишет про машины.Пишет про машины.Пишет про машины.Пишет про машины.Пишет про машины.Пишет про машины.Пишет про машины.Пишет про машины.Пишет про машины.Пишет про машины.Пишет про машины.Пишет про машины.
+                        {authorInfo?.description}
                     </p>
                 </div>
             </div>
             <div className={s.authorBooksContainer}>
                 <p className={s.authorBooksTitle}>Книги автора:</p>
-                <div className={s.authorBooksCards}>
-                    {books.map((book, index) => (<BookCard 
-                        key={index}
-                        title={book.title} 
-                        author={book.author} 
-                        coverImage={book.coverImage} 
-                        rating={book.rating} 
-                        toDirect={book.toDirect}
-                        classname={s.authorsBook}
-                    />
-                    ))}
-                </div>
+                {authorBooks.length > 0 ? (
+                    <div className={s.authorBooksCards}>
+                        {authorBooks.map((book, index) => (
+                            <BookCard
+                                key={index}
+                                title={book.title}
+                                author={book.author}
+                                coverImage={book.coverImage}
+                                rating={book.rating}
+                                toDirect={book.toDirect}
+                                classname={s.authorsBook}
+                            />
+                        ))}
+                    </div>
+                ) : (
+                    <div>У автора пока что нет книг.</div>
+                )}
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default AuthorPage;

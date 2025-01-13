81f779b8bc1e58fd6512366eae77d413e702b4f4
impronez <orzaev7312@gmail.com>
2025-01-13 15:56:22 +0300

    Добавил получение информации с сервера

--- frontend/src/pages/BookPage/BookPage.tsx
+++ frontend/src/pages/BookPage/BookPage.tsx
@@ -1,27 +1,82 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import BookReviews from '../../component/common/BookReviews/BookReviews';
 import s from './BookPage.module.css'
+import { BookApiClient } from '../../../api/ApiClient';
+import { GetBookInfoRequest } from '../../../api';
+import { useParams } from 'react-router-dom';
+import { ParsedBookInfo } from '../../types/BookTypes';
+import { parseAuthorInfoInBookResponse } from '../../utils/AuthorUtils';
+import { parseBookInfoResponse } from '../../utils/BookUtils';
+import { ParsedAuthorInfo } from '../../types/AuthorTypes';
+import { parseFeedbacksInBookResponse } from '../../utils/FeedbackUtils';
+import { ParsedFeedbackInfo } from '../../types/FeedbackTypes';
 
 const BookPage = () => {
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+
+    // TODO: добавление проверять через список книг, находящихся в библиотеке пользователя
     const [isAddedToRead, setIsAddedToRead] = useState(false);
     const changeBookToReadStateHandleClick = ({ isAdded }: { isAdded: boolean }) => {
         setIsAddedToRead(isAdded); 
     };
 
-    const genre = 's';
+    const [bookInfo, setBookInfo] = useState<ParsedBookInfo | null>(null);
+    const [authorInfo, setAuthorInfo] = useState<ParsedAuthorInfo[]>([]);
+    const [feedbackInfo, setFeedbackInfo] = useState<ParsedFeedbackInfo[]>([]);
+
+    const { id } = useParams<{ id: string }>();
+    const curBookId = id ? Number(id) : 0;
+    const request: GetBookInfoRequest = { bookID: curBookId};
+
+    useEffect(() => {
+        BookApiClient.getBookInfo(request)
+            .then((response) => {
+                setBookInfo(parseBookInfoResponse(response));
+                setAuthorInfo(parseAuthorInfoInBookResponse(response));
+                setFeedbackInfo(parseFeedbacksInBookResponse(response));
+                setLoading(false);
+            })
+            .catch((err) => {
+                setError('Не удалось загрузить данные. Попробуйте снова позже.');
+                setLoading(false);
+                console.error('Ошибка при запросе данных: ', err);
+            });
+    }, [])
+
+    if (loading) {
+        return <div>Загрузка...</div>
+    }
+
+    if (error) {
+        return (
+            <div>
+                <p>{error}</p>
+                <p>Попробуйте перезагрузить страницу</p>
+            </div>
+        );
+    }
 
     return (
         <div className={s.bookCardWrapper}>
             <div className={s.bookCard}>
-                <img className={s.bookCardImage} src={`/src/images/book/book.jpg`} />
+                <img className={s.bookCardImage} src={bookInfo?.coverImage} />
                 <div className={s.bookCardInfo}>
-                    <p className={s.bookCardTitle}>Луна в ореховой скорлупе</p>
-                    <div className={s.bookCardAuthor}>Автор:<a href='/author/0'> Анастасия Пикина</a></div>
+                    <p className={s.bookCardTitle}>{bookInfo?.title}</p>
+                    <div className={s.bookCardAuthor}>
+                        Автор:{" "}
+                        {authorInfo.map((author, index) => (
+                            <span key={author.id}>
+                            <a href={`/author/${author.id}`}>{`${author.firstName} ${author.lastName}`}</a>
+                            {index < authorInfo.length - 1 && ", "}
+                            </span>
+                        ))}
+                    </div>
                     <div className={s.bookCardAction}>
                         <div className={s.bookCardRatingInfo}>
                             <div className={s.bookCardRatingContainer}>
                                 <div className={s.bookCardRatingLogo} />
-                                <span className={s.bookCardRating}>5,0</span>
+                                <span className={s.bookCardRating}>{bookInfo?.rating}</span>
                             </div>
                         </div>
                         {!isAddedToRead ? 
@@ -34,17 +89,17 @@ const BookPage = () => {
                             <button className={s.bookCardDeleteBookButton} onClick={() => changeBookToReadStateHandleClick({ isAdded: true })}>Удалить из списка чтения</button>   
                         </div>}
                     </div>
-                    <p className={s.bookCardDescription}><span className={s.descrpitionTitle}>Жанр:</span> {genre}</p>
+                    <p className={s.bookCardDescription}><span className={s.descrpitionTitle}>Жанр:</span></p>
                     <div className={s.descriptionContainer}>
                         <span className={s.descrpitionTitle}>Описание</span>
                         <p className={s.bookCardDescription}>
-                        Луна в ореховой скорлупе» – весёлые сказки об озорном и неунывающем бельчонке Скоке, живущем в дупле на высоком-высоком дереве. Маленький Скок постоянно попадает в разные истории, которые становятся поводами для насмешек братьев, нравоучений старшей сестры, серьёзных разговоров с папой и безграничной любви мамы белки.
+                            {bookInfo?.description}
                         </p>
                     </div>
                 </div>
             </div>
             <div className={s.bookCardReviews}>
-                <BookReviews id='sd' />
+                <BookReviews feedbackInfo={feedbackInfo} />
             </div>
         </div>
     )

d308b059a2b4dbf7242f99114b69e1fd51ed7f50
impronez <orzaev7312@gmail.com>
2025-01-16 11:35:50 +0300

    Поправил страницу профиля

--- frontend/src/pages/ProfilePage/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage/ProfilePage.tsx
@@ -12,7 +12,7 @@ const ProfilePage = () => {
 
     const [showPassword, setShowPassword] = useState(false);
 
-    const handleChange = (e) => {
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const {name, value} = e.target;
         setFormData({...formData, [name]: value});
     };
@@ -40,67 +40,70 @@ const ProfilePage = () => {
                         Выйти
                     </button>
                 </div>
-                <form className={s.form}>
-                    <div className={s.inputWrapper}>
-                        <label className={s.label}>
-                            Имя
-                        </label>
-                        <input
-                            className={s.input}
-                            type="text"
-                            name="firstName"
-                            value={formData.firstName}
-                            onChange={handleChange}
-                            required
-                        />
+                <div className={s.infoContainer}>
+                    <div className={s.formContainer}>
+                        <form className={s.form}>
+                            <div className={s.inputWrapper}>
+                                <label className={s.label}>
+                                    Имя
+                                </label>
+                                <input
+                                    className={s.input}
+                                    type="text"
+                                    name="firstName"
+                                    value={formData.firstName}
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                            <div className={s.inputWrapper}>
+                                <label className={s.label}>
+                                    Фамилия
+                                </label>
+                                <input
+                                    className={s.input}
+                                    type="text"
+                                    name="lastName"
+                                    value={formData.lastName}
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                            <div className={s.inputWrapper}>
+                                <label className={s.label}>
+                                    E-mail
+                                </label>
+                                <input
+                                    className={s.input}
+                                    type="email"
+                                    name="email"
+                                    value={formData.email}
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                            <div className={s.inputWrapper}>
+                                <label className={s.label}>
+                                    Пароль
+                                </label>
+                                <input
+                                    className={s.input}
+                                    type={showPassword ? 'text' : 'password'}
+                                    name="password"
+                                    value={formData.password}
+                                    onChange={handleChange}
+                                    required
+                                />
+                                <div onClick={togglePasswordVisibility} className={showPassword ? s.eye : s.eyeClose}></div>
+                            </div>
+                        </form>
                     </div>
-                    <div className={s.inputWrapper}>
-                        <label className={s.label}>
-                            Фамилия
-                        </label>
-                        <input
-                            className={s.input}
-                            type="text"
-                            name="lastName"
-                            value={formData.lastName}
-                            onChange={handleChange}
-                            required
-                        />
+                    <div className={s.logotype}>
+                        <img className={s.avatar} alt={formData.firstName} src={formData.avatar}></img>
+                        <button type="button" onClick={handleAvatarChange}>
+                            Сменить аватар
+                        </button>
                     </div>
-                    <div className={s.inputWrapper}>
-                        <label className={s.label}>
-                            E-mail
-                        </label>
-                        <input
-                            className={s.input}
-                            type="email"
-                            name="email"
-                            value={formData.email}
-                            onChange={handleChange}
-                            required
-                        />
-                    </div>
-                    <div className={s.inputWrapper}>
-                        <label className={s.label}>
-                            Password
-                        </label>
-                        <input
-                            className={s.input}
-                            type={showPassword ? 'text' : 'password'}
-                            name="password"
-                            value={formData.password}
-                            onChange={handleChange}
-                            required
-                        />
-                        <div onClick={togglePasswordVisibility} className={showPassword ? s.eye : s.eyeClose}></div>
-                    </div>
-
-                </form>
-                <div className={s.logotype}>
-                    <img className={s.avatar} alt={formData.firstName} src={formData.avater}></img>
-                    <button type="button" onClick={handleAvatarChange}>
-                        Сменить аватар
-                    </button>
                 </div>
             </div>
 

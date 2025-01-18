a2e0d368d7c18f3a960db27af3bb9b552329f6bb
impronez <orzaev7312@gmail.com>
2025-01-18 10:52:57 +0300

    Сделал страницу поиска

--- frontend/src/pages/SearchPage/SearchPage.tsx
+++ frontend/src/pages/SearchPage/SearchPage.tsx
@@ -1,10 +1,144 @@
+import { useSearchParams } from 'react-router-dom';
+import s from './SearchPage.module.css';
+import { useEffect, useState, useCallback } from 'react';
+import { BookApiClient } from '../../../api/ApiClient';
+import { ParsedBookCard } from '../../types/BookTypes';
+import { parseBookCardsResponse } from '../../utils/BookUtils';
+import { ArticleData, AuthorData } from '../../../api';
+import BookCard from '../../component/common/BookCard/BookCard';
 
 const SearchPage = () => {
-    return (
-        <div>
+    const [searchParams, setSearchParams] = useSearchParams();
+    const query = searchParams.get('q');
+    const initialCategory = searchParams.get('category') || 'books';
+    const initialPage = parseInt(searchParams.get('page') || '1', 10);
 
+    const [books, setBooks] = useState<ParsedBookCard[]>([]);
+    const [articles, setArticles] = useState<ArticleData[]>([]);
+    const [authors, setAuthors] = useState<AuthorData[]>([]);
+    const [page, setPage] = useState(initialPage);
+    const [loading, setLoading] = useState(false);
+    const [hasMore, setHasMore] = useState(true);
+    const [selectedCategory, setSelectedCategory] = useState<string>(initialCategory);
+
+    const loadSearchResults = useCallback(async () => {
+        if (!query) return;
+        setLoading(true);
+        try {
+            // TODO: добавить пагинацию
+            const response = await BookApiClient.searchItems({ searchItemsRequest: { searchString: query } });
+            if (response) {
+                setBooks((prevBooks) => [...prevBooks, ...parseBookCardsResponse(response)]);
+                setArticles((prevArticles) => [...prevArticles, ...(response.articles || [])]);
+                setAuthors((prevAuthors) => [...prevAuthors, ...(response.authors || [])]);
+                setHasMore(((response.books?.length ?? 0) > 0)
+                    || ((response.articles?.length ?? 0) > 0)
+                    || ((response.authors?.length ?? 0) > 0));
+            }
+        } catch (err) {
+            console.error('Failed to fetch search items: ', err);
+        } finally {
+            setLoading(false);
+        }
+    }, [query, page]);
+
+    useEffect(() => {
+        loadSearchResults();
+    }, [query, page, loadSearchResults]);
+
+    useEffect(() => {
+        setSearchParams({ q: query || '', category: selectedCategory, page: page.toString() });
+    }, [query, selectedCategory, page, setSearchParams]);
+
+    const handleLoadMore = () => {
+        setPage((prevPage) => prevPage + 1);
+    };
+
+    const handleCategoryClick = (category: string) => {
+        setSelectedCategory(category);
+        setPage(1);
+        setBooks([]);
+        setArticles([]);
+        setAuthors([]);
+    };
+
+    return (
+        <div className={s.container}>
+            <div className={s.resultsContainer}>
+                <p className={s.searchTitle}>Результаты поиска «{query}»</p>
+                <div className={s.searchCategoriesContainer}>
+                    <div
+                        className={`${s.searchCategory} ${selectedCategory === 'books' ? s.selected : ''}`}
+                        onClick={() => handleCategoryClick('books')}
+                    >
+                        <div className={s.searchCategoryName}>Книги</div>
+                    </div>
+                    <div
+                        className={`${s.searchCategory} ${selectedCategory === 'articles' ? s.selected : ''}`}
+                        onClick={() => handleCategoryClick('articles')}
+                    >
+                        <div className={s.searchCategoryName}>Статьи</div>
+                    </div>
+                    <div
+                        className={`${s.searchCategory} ${selectedCategory === 'authors' ? s.selected : ''}`}
+                        onClick={() => handleCategoryClick('authors')}
+                    >
+                        <div className={s.searchCategoryName}>Авторы</div>
+                    </div>
+                </div>
+                <div className={s.searchCategoryItemsContainer}>
+                    {selectedCategory === 'books' && (
+                        <div className={s.searchBooksContainer}>
+                            {books.map((book, key) => (
+                                <BookCard
+                                    key={key}
+                                    title={book.title}
+                                    author={book.author}
+                                    rating={book.rating}
+                                    toDirect={book.toDirect}
+                                    coverImage={book.coverImage}
+                                    classname={`${s.searchBook}`}
+                                />
+                            ))}
+                        </div>
+                    )}
+                    {selectedCategory === 'authors' && (
+                        <div className={s.searchItemsContainer}>
+                            {authors.map((author, key) => (
+                                <a className={s.categoryLink} key={key} href={`/author/${author.id}`}>
+                                    <div className={s.searchAuthor}>
+                                        <p className={`${s.authorName} ${s.textEllipsis}`}>{author.firstName} {author.lastName}</p>
+                                        <p className={`${s.authorDescription} ${s.textEllipsis}`}>{author.description}</p>
+                                    </div>
+                                </a>
+                            ))}
+                        </div>
+                    )}
+                    {selectedCategory === 'articles' && (
+                        <div className={s.searchItemsContainer}>
+                            {articles.map((article, key) => (
+                                <a className={s.categoryLink} key={key} href={`/article/${article.id}`}>
+                                    <div className={s.searchArticle}>
+                                        <p className={`${s.articleTitle} ${s.textEllipsis}`}>{article.title}</p>
+                                        <p className={`${s.articleAuthor} ${s.textEllipsis}`}>{article.author.firstName} {article.author.lastName}</p>
+                                        <p className={`${s.articlePublishDate} ${s.textEllipsis}`}>{article.publishDate}</p>
+                                    </div>
+                                </a>
+                            ))}
+                        </div>
+                    )}
+                </div>
+            </div>
+            {loading && <p>Loading...</p>}
+            {hasMore && !loading && (
+                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                    <button className={s.loadMoreButton} onClick={handleLoadMore}>
+                        Загрузить еще
+                    </button>
+                </div>
+            )}
         </div>
-    )
-}
+    );
+};
 
 export default SearchPage;

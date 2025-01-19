958f5f79aef90a9978a27de58644288e2ebc9602
SantiXN <santixm123@gmail.com>
2025-01-19 17:47:34 +0300

    Поправил создание автора, добавил вывод ошибок

--- backend/pkg/infrastructure/transport/errors.go
+++ backend/pkg/infrastructure/transport/errors.go
@@ -7,6 +7,7 @@ import (
 	"booklib/pkg/domain/model"
 	"context"
 	"errors"
+	"fmt"
 	"net/http"
 )
 
@@ -16,6 +17,7 @@ func NewPublicAPIErrorsMiddleware() api.StrictMiddlewareFunc {
 		return func(ctx context.Context, w http.ResponseWriter, r *http.Request, args interface{}) (interface{}, error) {
 			resp, err := f(ctx, w, r, args)
 			if err != nil {
+				fmt.Println(err.Error())
 				if errors.Is(err, query.ErrUserNotFound) ||
 					errors.Is(err, model.ErrAuthorNotFound) ||
 					errors.Is(err, model.ErrBookNotFound) ||

925ecf2eca8678d44df91ab5c8ba299c8ac6d8b2
impronez <orzaev7312@gmail.com>
2025-01-23 11:34:50 +0300

    Поменял тип ссылки

--- frontend/src/component/Footer/Footer.tsx
+++ frontend/src/component/Footer/Footer.tsx
@@ -1,13 +1,14 @@
+import { Link } from "react-router-dom";
 import logo from "../../images/svg/logotype.svg"
 import s from "./Footer.module.css"
 
 const Footer = () => {
     return (
         <footer className={s.footer}>
-            <a href="/" className={s.logoLink}>
+            <Link to="/" className={s.logoLink}>
                 <p className={s.logoText}>© BookLib 2024</p>
                 <img src={logo} className={s.logoImage} alt="logo"></img>
-            </a>
+            </Link>
         </footer>
     )
 }

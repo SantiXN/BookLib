a3949ca9e6fa3b3a3a0a70bc7b095f8217216682
impronez <orzaev7312@gmail.com>
2025-01-23 14:00:45 +0300

    Поправил отображение для админа и редактора

--- frontend/src/pages/AdminPages/FunctionalWindows/RemoveArticleBlock/RemoveArticleBlock.tsx
+++ frontend/src/pages/AdminPages/FunctionalWindows/RemoveArticleBlock/RemoveArticleBlock.tsx
@@ -1,17 +1,20 @@
 import { useEffect, useRef, useState } from 'react';
 import s from '../FunctionalWindow.module.css'
 import useApi from '../../../../../api/ApiClient';
-import { ArticleInfo } from '../../../../../api';
+import { ArticleData, ArticleInfo } from '../../../../../api';
 
 interface BlockProps {
     isOpen: string | null;
     onClose: () => void;
+    isAdmin: boolean;
 }
 
-const RemoveArticleBlock: React.FC<BlockProps> = ({ isOpen, onClose }) => {
+const RemoveArticleBlock: React.FC<BlockProps> = ({ isOpen, onClose, isAdmin }) => {
     const { ArticleApi } = useApi();
 
     const containerRef = useRef<HTMLDivElement>(null);
+
+    const [articles, setArticles] = useState<ArticleData[]>([]);
 
     const [articleInfo, setArticleInfo] = useState<ArticleInfo | null>(null);
     const [selectedArticleID, setSelectedArticleID] = useState<number | null>(null);
@@ -22,6 +25,18 @@ const RemoveArticleBlock: React.FC<BlockProps> = ({ isOpen, onClose }) => {
             onClose();        
         }
     };
+
+    useEffect(() => {
+        if (!isAdmin) {
+            ArticleApi.managementArticles()
+                .then((response) => {
+                    if (response.articles) {
+                        setArticles(response.articles)
+                    }
+                })
+                .catch((err) => alert(`Ошибка получения статей: ${err}`));
+        }
+    }, [])
 
     const handleEscapeKey = (event: KeyboardEvent) => {
         if (event.key === 'Escape') {
@@ -45,6 +60,12 @@ const RemoveArticleBlock: React.FC<BlockProps> = ({ isOpen, onClose }) => {
         const articleId = Number(e.target.value);
         setSelectedArticleID(articleId);
     };    
+
+    const handleArticleChangeFromSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        const articleId = Number(e.target.value);
+        setSelectedArticleID(articleId);
+        handleSearchArticle();
+    }
 
     const handleSearchArticle = () => {
         if (!selectedArticleID) return;
@@ -84,24 +105,39 @@ const RemoveArticleBlock: React.FC<BlockProps> = ({ isOpen, onClose }) => {
                 </div>
                 <div className={s.menuContainer}>
                     <form className={s.form} onSubmit={handleSubmit}>
-                     <div className={`${s.formFieldContainer} ${s.removeInputContainer}`}>
-                            <label className={s.formLabel} htmlFor='articleId'>Статья</label>
-                            <input
-                                className={`${s.input} ${s.autoWidth}`}
-                                id='articleId'
-                                type='number'  
-                                value={selectedArticleID || ''}
-                                onChange={(value) => handleArticleChange(value)}
-                            />
-                            <button
-                                type="button"
-                                onClick={handleSearchArticle}
-                                disabled={!selectedArticleID}
-                                className={`${s.button} ${!selectedArticleID ? s.disabledButton : ''}`}
-                            >
-                                Поиск
-                            </button>
-                        </div>
+                        {!isAdmin && (
+                            <div className={s.formFieldContainer}>
+                                <label className={s.formLabel} htmlFor='author'>Статья</label>
+                                <select id="author" className={s.input} value={selectedArticleID || ''} onChange={handleArticleChangeFromSelect}>
+                                    <option value='' disabled>Выберите...</option>
+                                    {articles.map((art, index) => (
+                                        <option key={index} value={art.id}>
+                                            {art.title} (id={art.id})
+                                        </option>
+                                    ))}
+                                </select>
+                            </div>
+                        )}
+                        {isAdmin && (
+                            <div className={`${s.formFieldContainer} ${s.inputContainerFromId}`}>
+                                <label className={s.formLabel} htmlFor='articleId'>ID статьи</label>
+                                <input
+                                    className={`${s.input} ${s.autoWidth}`}
+                                    id='articleId'
+                                    type='number'  
+                                    value={selectedArticleID || ''}
+                                    onChange={(value) => handleArticleChange(value)}
+                                />
+                                <button
+                                    type="button"
+                                    onClick={handleSearchArticle}
+                                    disabled={!selectedArticleID}
+                                    className={`${s.button} ${!selectedArticleID ? s.disabledButton : ''}`}
+                                >
+                                    Поиск
+                                </button>
+                            </div>
+                        )}
                         {articleInfo && (
                             <div>
                                 <a href={`/article/${selectedArticleID}`}><p>{articleInfo.title}</p></a>

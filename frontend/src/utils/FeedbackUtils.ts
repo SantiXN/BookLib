8e0c7b2ae249bde3a97462857e4bc0fb65df0623
impronez <orzaev7312@gmail.com>
2025-01-26 13:18:44 +0300

    Убрал лишнее

--- frontend/src/utils/FeedbackUtils.ts
+++ frontend/src/utils/FeedbackUtils.ts
@@ -1,20 +1,5 @@
-import { ListBookFeedbackResponseData, SaveBookFeedbackOperationRequest, SaveBookFeedbackRequest } from "../../api";
-import { ParsedFeedbackInfo } from "../types/FeedbackTypes";
+import { SaveBookFeedbackOperationRequest, SaveBookFeedbackRequest } from "../../api";
 
-
-export function parseFeedbacksInBookResponse(response: ListBookFeedbackResponseData): ParsedFeedbackInfo[] {
-    if (!response || !response.feedback) {
-        return [];
-    }
-    
-    return response.feedback.map((feedback) => ({
-        id: feedback.id,
-        userID: feedback.userID,
-        starCount: feedback.starCount,
-        postedAt: feedback.postedAt,
-        comment: feedback.comment
-    }))
-}
 
 export function getFeedbackOperationRequest(bookID: number, starCount: number, comment?: string) : SaveBookFeedbackOperationRequest {
     const feedbackRequest: SaveBookFeedbackRequest = {starCount: starCount, comment: comment};
